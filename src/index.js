c3e3d52464f73cbf996ca7322a7c1079b5e56243
mark <markpackham1@gmail.com>
2022-03-28 14:54:28 +0100

    demo handling of svg image & images in general
    https://youtu.be/IZGNcSuwBZs?t=2001

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 // import { v4 as uuidv4 } from "uuid";
 import generateJoke from "./generateJoke";
 import "./styles/main.scss";
+import laughing from "./assets/laughing.svg";
 
 console.log(generateJoke());
