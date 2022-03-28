0c54a6a6ad3447f9a29343dc80729d9645392728
mark <markpackham1@gmail.com>
2022-03-28 15:21:35 +0100

    fetch jokes on click
    https://youtu.be/IZGNcSuwBZs?t=2272

--- src/index.js
+++ src/index.js
@@ -6,4 +6,7 @@ import laughing from "./assets/laughing.svg";
 const laughImg = document.getElementById("laughImg");
 laughImg.src = laughing;
 
-console.log(generateJoke());
+const jokeBtn = document.getElementById("jokeBtn");
+jokeBtn.addEventListener("click", generateJoke);
+
+generateJoke();
