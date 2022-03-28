226cd4945b2acdf645326e7a970e5ae08b57106b
mark <markpackham1@gmail.com>
2022-03-28 14:06:07 +0100

    set devserver port and ability to open automatically on "npm run dev"
    https://youtu.be/IZGNcSuwBZs?t=1525

--- webpack.config.js
+++ webpack.config.js
@@ -14,6 +14,11 @@ module.exports = {
     static: {
       directory: path.resolve(__dirname, "dist"),
     },
+    port: 3000,
+    open: true,
+    hot: true,
+    compress: true,
+    historyApiFallback: true,
   },
   module: {
     rules: [
